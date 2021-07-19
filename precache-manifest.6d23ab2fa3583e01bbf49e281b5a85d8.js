fea1774ca3a0a8785acff08c104c307aa67b3303
Yehor Merzlenko <yehor.merzlenko@gmail.com>
2021-07-19 17:27:15 +0300

    Updates

---
+++ precache-manifest.6d23ab2fa3583e01bbf49e281b5a85d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659af3a858db7debfdcc189baf5bc2a5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b43e85a6136d8d43eeb8",
+    "url": "./static/css/main.edbfb54e.chunk.css"
+  },
+  {
+    "revision": "edcc8acd3c83803ea5af",
+    "url": "./static/js/2.18aeaae4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18aeaae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43e85a6136d8d43eeb8",
+    "url": "./static/js/main.dd9d3ab3.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
